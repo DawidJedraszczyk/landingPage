33dcac0248fcfc74012b76da33cfba1e234373ec
DawidJedraszczyk <dawidjedraszczyk01@gmail.com>
2023-04-17 22:58:25 +0200

    added animations

--- js/index.js
+++ js/index.js
@@ -36,7 +36,38 @@ fadersRight.forEach((fader) => {
   appearOnScroll.observe(fader);
 });
 
-function scrollToDiv(divName) {
-  var div = document.getElementById(divName);
-  div.scrollIntoView();
+$(document).ready(function () {
+  $("a.smooth-scroll").click(function (e) {
+    e.preventDefault();
+    const target = $($(this).attr("href"));
+    if (target.length) {
+      $("html, body").animate(
+        {
+          scrollTop: target.offset().top,
+        },
+        2000
+      );
+    }
+  });
+});
+
+function showMentors() {
+  mentors.classList.add("active");
+  developers.classList.remove("active");
+  const elements = document.querySelectorAll(".participant");
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].classList.add("changeVis");
+  }
 }
+function showDevs() {
+  mentors.classList.remove("active");
+  developers.classList.add("active");
+  const elements = document.querySelectorAll(".participant");
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].classList.remove("changeVis");
+  }
+}
+let developers = document.getElementById("Devs");
+let mentors = document.getElementById("Mentors");
+mentors.addEventListener("click", showMentors);
+developers.addEventListener("click", showDevs);
