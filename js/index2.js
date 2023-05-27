d56030b011e03eaee307a51262d8f999dad61d7c
DawidJedraszczyk <dawidjedraszczyk01@gmail.com>
2023-05-27 10:48:28 +0200

    finished respons

--- js/index2.js
+++ js/index2.js
@@ -1,47 +1,3 @@
-// header background after 1vh -100px scroll
-
-window.addEventListener("scroll", function () {
-  const navbar = document.querySelector("nav");
-  const scrollHeight = window.pageYOffset;
-  if (scrollHeight > window.innerHeight - 100) {
-    navbar.classList.add("GreenGradient");
-  } else {
-    navbar.classList.remove("GreenGradient");
-  }
-});
-
-/*  ANIMATION FOR #TEAM */
-function showMentors() {
-  document.getElementById("first-mentor").style.opacity = "1";
-  mentors.classList.add("active");
-  developers.classList.remove("active");
-  const elements = document.querySelectorAll(".participant");
-  for (let i = 0; i < elements.length; i++) {
-    elements[i].classList.remove("fade-in");
-    elements[i].classList.add("changeVis");
-  }
-  document.getElementById("first-dev").style.opacity = "0";
-  document.getElementById("second-dev").style.opacity = "0";
-  document.getElementById("third-dev").style.opacity = "0";
-}
-function showDevs() {
-  document.getElementById("first-dev").style.opacity = "1";
-  document.getElementById("second-dev").style.opacity = "1";
-  document.getElementById("third-dev").style.opacity = "1";
-  mentors.classList.remove("active");
-  developers.classList.add("active");
-  const elements = document.querySelectorAll(".participant");
-  for (let i = 0; i < elements.length; i++) {
-    elements[i].classList.remove("fade-in");
-    elements[i].classList.remove("changeVis");
-  }
-  document.getElementById("first-mentor").style.opacity = "0";
-}
-let developers = document.getElementById("devsBtn");
-let mentors = document.getElementById("mentorsBtn");
-mentors.addEventListener("click", showMentors);
-developers.addEventListener("click", showDevs);
-
 /*  SCROLL TO DIV  */
 $(document).ready(function () {
   $("a.smooth-scroll").click(function (e) {
@@ -59,12 +15,12 @@ $(document).ready(function () {
 });
 
 /* menu */
-let menuCheckbox = document.getElementById("menu-checkbox");
-let contactBtn = document.getElementById("contact-btn");
+const menuCheckbox = document.getElementById("menu-checkbox");
+const contactBtn = document.getElementById("contact-btn");
 const menuElement = document.createElement("div");
 menuElement.classList.add("menu");
 menuElement.innerHTML = `
-<ul>
+<ul id="menu-list">
   <li id="home-btn">
     <a href="#hero" class="smooth-scroll">Home</a>
   </li>
@@ -74,18 +30,100 @@ menuElement.innerHTML = `
   <li id="team-btn">
     <a href="#team" class="smooth-scroll">Team</a>
   </li>
-  <li id="team-btn">
+  <li id="contact-btn-menu">
     <a href="#contact" class="smooth-scroll">Contact us</a>
   </li>
 </ul>`;
 menuCheckbox.addEventListener("click", function () {
-  if (window.innerWidth < 1300) {
-    if (menuCheckbox.checked) {
-      document.body.appendChild(menuElement);
-      contactBtn.style.opacity = "0";
-    } else {
-      document.body.removeChild(menuElement);
-      contactBtn.style.opacity = "1";
-    }
-  }
+  window.innerWidth < 850 && menuCheckbox.checked
+    ? document.body.appendChild(menuElement)
+    : document.body.removeChild(menuElement);
+  menuEventListener();
 });
+
+// hide menu after click href
+
+const hideMenu = () => {
+  menuElement.parentNode === document.body &&
+    document.body.removeChild(menuElement);
+};
+const menuEventListener = () => {
+  menuElement.parentNode === document.body &&
+    menuElement.addEventListener("click", hideMenuAndUncheckCheckbox);
+};
+
+const hideMenuAndUncheckCheckbox = () => {
+  hideMenu();
+  menuCheckbox.checked = false;
+};
+
+// header background after 1vh -100px scroll
+
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector("nav");
+  const scrollHeight = window.pageYOffset;
+  scrollHeight > window.innerHeight - 100
+    ? navbar.classList.add("GreenGradient")
+    : navbar.classList.remove("GreenGradient");
+});
+
+/*  ANIMATION FOR #TEAM */
+const developers = document.getElementById("devsBtn");
+const mentors = document.getElementById("mentorsBtn");
+const developersId = ["first-dev", "second-dev", "third-dev"];
+const mentorsId = ["first-mentor"];
+const participants = Array.from(document.querySelectorAll(".participant"));
+
+const changeOpacity = (elem, value) => {
+  document.getElementById(elem).style.opacity = value;
+};
+
+const changePurpleButton = (clicked) => {
+  clicked === mentors
+    ? (mentors.classList.add("active"), developers.classList.remove("active"))
+    : (mentors.classList.remove("active"), developers.classList.add("active"));
+};
+
+const moveTeamAndShow = (nextToShow) => {
+  nextToShow === "mentors"
+    ? changeClassesOfParticipantsAndShow("mentors")
+    : changeClassesOfParticipantsAndShow("developers");
+};
+
+const changeClassesOfParticipantsAndShow = (position) => {
+  position === "mentors"
+    ? participants.map(
+        (elem) => (
+          elem.classList.remove("fade-in"), elem.classList.add("changeVis")
+        )
+      )
+    : participants.map(
+        (elem) => (
+          elem.classList.remove("fade-in"), elem.classList.remove("changeVis")
+        )
+      );
+};
+
+const showMentors = () => {
+  mentorsId.map((elem) => {
+    changeOpacity(elem, 1);
+  });
+  changePurpleButton(mentors);
+  moveTeamAndShow("mentors");
+  developersId.map((elem) => {
+    changeOpacity(elem, 0);
+  });
+};
+const showDevs = () => {
+  developersId.map((elem) => {
+    changeOpacity(elem, 1);
+  });
+  changePurpleButton(developers);
+  moveTeamAndShow("developers");
+  mentorsId.map((elem) => {
+    changeOpacity(elem, 0);
+  });
+};
+
+mentors.addEventListener("click", showMentors);
+developers.addEventListener("click", showDevs);
